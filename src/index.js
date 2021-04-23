6331cbb274a1c0cd8241838d3502e929640c4c11
Sajib132 <sajibb813@gmail.com>
2021-04-23 15:28:59 +0530

    Implementation of Map Method

--- src/index.js
+++ src/index.js
@@ -6,41 +6,24 @@ import reportWebVitals from './reportWebVitals';
 import Sdata from './Sdata';
 
 
-console.log(Sdata[0]);
+function ncard(val){
+  return (
+    <App 
+    imgsrc={val.imgsrc}
+    title={val.title} 
+    sname={val.sname} 
+    link={val.link} />
+  ); 
+}
 
 ReactDOM.render(
   <>
   <h1 className="heading+style">List of 5 Netflix Series 2021</h1>
-    <App 
-    imgsrc={Sdata[0].imgsrc}
-    title={Sdata[0].title} 
-    sname={Sdata[0].sname} 
-    link={Sdata[0].link} />
-
-  <App 
-    imgsrc={Sdata[1].imgsrc}
-    title={Sdata[1].title} 
-    sname={Sdata[1].sname} 
-    link={Sdata[1].link} />
+   
 
 
-    <App 
-    imgsrc={Sdata[2].imgsrc}
-    title={Sdata[2].title}
-     sname={Sdata[2].sname} 
-     link={Sdata[2].link} />
+    {Sdata.map(ncard)};
 
-    <App 
-    imgsrc={Sdata[3].imgsrc}
-    title={Sdata[3].title}
-     sname={Sdata[3].sname}
-      link={Sdata[3].link} />
-
-    <App 
-    imgsrc={Sdata[4].imgsrc}
-    title={Sdata[4].title}
-     sname={Sdata[4].sname}
-      link={Sdata[4].link} />
   </>,
   document.getElementById('root')
 );
